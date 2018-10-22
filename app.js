6cf81f6f5ecba0ad4b8cd0df47970679df87ac33
Prithul Singh <ccarmichael.zork.el@gmail.com>
2018-10-22 16:10:37 +0530

    Deploying to Heroku.

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const hbs = require('express-handlebars');
 
 const app = express();
 
-
-console.log(path.join(__dirname, '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine('hbs', hbs({extname:'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/'}));
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'hbs');
 
