13e7c304e29d7f60dff74f0c08025d2e97139bab
Prithul Singh <ccarmichael.zork.el@gmail.com>
2018-10-23 21:45:04 +0530

    OAuth workflow working since oauth Package added.

--- app.js
+++ app.js
@@ -1,96 +1,82 @@
 const express = require('express');
 const path = require('path');
-const http = require('http');
-const https = require('https');
 const bodyParser = require('body-parser');
 const hbs = require('express-handlebars');
-const API_KEY = '65a51c4b';
-const OMDB_ID = 'tt3896198';
-const params = require('./params');
+const oauth = require('oauth');
+
+consumer_key = '0yUiaDnzWdkPGYYSTydn1o1n2';
+consumer_secret = 'ZANua1QbkqqtmGbgdJ4nKbp7kJQx4borqJecI2vDgbMGqckqij';
+reqUrl = "https://api.twitter.com/oauth/request_token";
+accessUrl = "https://api.twitter.com/oauth/access_token";
+cbUrl = "https://stormy-lowlands-87826.herokuapp.com/twitter/cb";
+credentialUrl = "https://api.twitter.com/1.1/account/verify_credentials.json"
+var oauth_token;
+var oauth_token_secret;
+var access_token;
+var access_token_secret;
+var UserData;
+
+var consumer = new oauth.OAuth(reqUrl, accessUrl, consumer_key, consumer_secret, '1.0A', cbUrl, "HMAC-SHA1");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'hbs');
 
-var options = {
-    method: 'GET',
-    path: '',
-    headers: {
-        Host: 'www.omdbapi.com'
-    }
-};
-
-/* app.get('/', (req, res) => {
-    const reqObj = {
-        name: 'doctor who'
-    };
-    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${reqObj.name}&apikey=${API_KEY}`);
-    options.protocol = 'http:'
-    options.host = 'www.omdbapi.com'
-    options.path = encodeURI(`/?t=${reqObj.name}&apikey=${API_KEY}`);
-    http.request(options, (responseFromApi) => {
-        console.log(responseFromApi.statusCode);
-        let completeResponse = '';
-        responseFromApi.on('data', (chunk) => {
-            completeResponse += chunk;
-        });
-
-        responseFromApi.on('end', () => {
-            const listData = JSON.parse(completeResponse);
-            res.render('home', {
-                title: listData.Title,
-                year: listData.Released,
-                director: listData.Director,
-                imgUrl: listData.Poster
-            });
-        }, (error) => {
-            res.status(400).json(error);
-        });
-    }).end();
-}); */
-
 app.get('/twitter', (req, res) => {
-    const reqUrl = encodeURIComponent(`https://api.twitter.com/oauth/request_token`);
-    options.protocol = 'https:'
-    options.host = 'api.twitter.com'
-    options.path = '/oauth/request_token';
-    options.headers.Host = 'api.twitter.com';
-    options.method = 'POST';
-    const callbackUrl = `https://stormy-lowlands-87826.herokuapp.com/twitter/callback`;
-    const Params = params(options.method, reqUrl, callbackUrl);
-    Params.oauth_callback = callbackUrl;
-    var headers = Object.keys(Params)
-                    .map((key) => {
-                        let encVal = encodeURIComponent(Params[key]);
-                        return `${key}="${encVal}"`;
-                    })
-                    .join(', ');
-    headers = `OAuth ${headers}`;
-    options.headers.Authorization = headers;
-    options.headers.Accept = '*/*';
-    console.log(headers);
-    https.request(options, (responseFromApi) => {
-        let status = responseFromApi.statusCode;
-        let respHeaders = responseFromApi.headers;
-        let completeResponse = '';
-        responseFromApi.on('data', (chunk) => {
-            completeResponse += chunk;
-        });
+    consumer.getOAuthRequestToken((err, oauthToken, oauthTokenSecret, results) => {
+        if (err) res.status(401).json(err);
+        else {
+            oauth_token = oauthToken;
+            oauth_token_secret = oauthTokenSecret;
+            res.redirect("https://api.twitter.com/oauth/authorize?oauth_token=" + oauth_token);
+        }
+    });
+});
 
-        responseFromApi.on('end', () => {
-            const listData = JSON.parse(completeResponse);
-            console.log(completeResponse);
-            res.status(200).json({ data: listData, status: status, headers: respHeaders});
-        }, (error) => {
-            res.status(404).json(error);
+app.get('/twitter/cb', (req, res) => {
+    if (req.query.oauth_token == oauth_token) {
+        consumer.getOAuthAccessToken(oauth_token, oauth_token_secret, req.query.oauth_verifier, (err, accessToken, accessTokenSecret, results) => {
+            if (err) res.status(401).json(err);
+            else {
+                access_token = accessToken;
+                access_token_secret = accessTokenSecret;
+                res.redirect('/');
+            }
         });
-    }).end();
+    } else res.send(401).json('something went wrong!!');
+});
+
+app.get('/', (req, res) => {
+    if (access_token) {
+        consumer.get(credentialUrl, access_token, access_token_secret, (err, data, response) => {
+            if (err) {
+                res.status(err.statusCode).json(err.data);
+            } else {
+                UserData = JSON.parse(data);
+                res.render('home', { msg: "See Followers List", link: "/followers" });
+            }
+        });
+    } else res.render('home', { msg: "Log In to Twitter", link: "/twitter" });
+});
+
+app.get('/followers', (req, res) => {
+    if (access_token) {
+        let followersUrl = `https://api.twitter.com/1.1/followers/list.json?cursor=-1&screen_name=${UserData.screen_name}&skip_status=true&include_user_entities=false`;
+        consumer.get(followersUrl, access_token, access_token_secret, (err, data, response) => {
+            if (err) {
+                res.status(err.statusCode).json(err.data);
+            } else {
+                let followersData = JSON.parse(data);
+                res.render('home', { followers: true, list: followersData.users });
+            }
+        });
+    } else res.redirect('/');
 });
 
 module.exports = app;
